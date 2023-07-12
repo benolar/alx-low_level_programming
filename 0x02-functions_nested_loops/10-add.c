44ecef648f66860c12d32e48051bd7b939f965c8
benolar <fullstack.benolar@gmail.com>
2023-07-12 10:21:04 -0700

    quick fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,6 +9,6 @@
 int add(int n1, int n2)
 {
 	int result = n1 + n2;
-	
+
 	return (result);
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * print_to_98 - Prints all natural numbers from n to 98,
  * followed by a new line.
