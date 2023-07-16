441138fb50c00288c1da7007c2a5a1dd4c503aec
benolar <fullstack.benolar@gmail.com>
2023-07-15 20:09:31 -0700

    debugging completed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,31 +5,37 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: 0 if successful.  if not, 1
+ * Return: largest number
  */
 
-int main(void)
+int largest_number(int a, int b, int c)
 {
-    int a = 23;
-    int b = 15;
-    int c = 75;
-    
-    if (a > b && b > c || a > c && c > b || a > b && a > c)
-    {
-        printf("%d is the largest number\n", a);
-    }
-    else if (b > a && a > c || b > c && c > a || b > a && b > c)
-    {
-        printf("%d is the largest number\n", b);
-    }
-    else if (c > a && a > b || c > b && b > a || c > a && c > b)
-    {
-        printf("%d is the largest number\n", c);
-    }
-    else
-    {
-        printf("all numbers are equall\n");
-    }
-
-    return (0);
+	int largest;
+	
+	if ((a > b && b > c) || (a > c && c > b) || (a > b && a > c))
+	{
+		largest = a;
+	}
+	else if ((b > a && a > c) || (b > c && c > a) || (b > a && b > c))
+	{
+		largest = b;
+	}
+	else if ((c > a && a > b) || (c > b && b > a) || (c > a && c > b))
+	{
+		largest = c;
+	}
+	else if (a == b && a > c || a == c && a > b)
+	{
+		largest = a;
+	}
+	else if (b == c && b > a)
+	{
+		largest = b;
+	}
+	else
+	{
+		printf("all numbers are equall\n");
+	}
+	
+	return (largest);
 }
