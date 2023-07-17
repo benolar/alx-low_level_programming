82519d0e0dc1c6e3eef7daed8cfc669c3b8fb38d
benolar <fullstack.benolar@gmail.com>
2023-07-17 12:00:23 -0700

    Fixed code by adding parenthesis after && within || operator

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,25 +10,29 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 	
-	if (a > b && b > c || a > c && c > b || a > b && a > c)
+	if ((a > b && b > c) || (a > c && c > b) || (a > b && a > c))
 	{
 		largest = a;
 	}
-	else if (b > a && a > c || b > c && c > a || b > a && b > c)
+	else if ((b > a && a > c) || (b > c && c > a) || (b > a && b > c))
 	{
 		largest = b;
 	}
-	else if (c > a && a > b || c > b && b > a || c > a && c > b)
+	else if ((c > a && a > b) || (c > b && b > a) || (c > a && c > b))
 	{
 		largest = c;
 	}
-	else if (a == b && a > c || a == c && a > b)
+	else if ((a == b && a > c) || (a == c && a > b))
 	{
 		largest = a;
 	}
-	else
+	else if (b == c && b > a)
 	{
 		largest = b;
+	}
+	else
+	{
+		largest = a;
 	}
 	
 	return (largest);
